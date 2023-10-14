9c50454e8fba78a57b9986e5945bfe6f2a6b61ef
khorshed-aspire <khorshed@aspire-intel.com>
2023-10-14 19:22:13 +0600

    User CRUD with postgres

---
+++ src/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(user: Partial<User>) {
+    console.log(user);
+    const newuser = this.userRepository.create({ ...user });
+    return this.userRepository.save(newuser);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
